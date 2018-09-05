221f3472d1857b29311a7d9bc93ee30f38d4a88b
Vasileios Karakasis <karakasis@cscs.ch>
2018-09-05 15:23:01 +0200

    Fix CI job file.

--- ci/cscs-daint-gpu.sh
+++ ci/cscs-daint-gpu.sh
@@ -1,8 +1,10 @@
-#!/bin/bash
+#!/bin/bash -l
 #SBATCH --job-name='arbor-ci'
 #SBATCH --partition=cscsci
 #SBATCH -C gpu
 
 rfm_exec=$1
+
+module load reframe
 
 $rfm_exec --system=daint:gpu -C ci/rfm-config.py -c ci/arbor_tests.py --prefix=$SCRATCH/rfm-stage --exec-policy=async -r
